73d494a648ab79b766068301bb6de6f1a8f6908c
PayneJoe <joepayne@163.com>
2025-11-06 13:46:12 +0800

    add window method of binary polynomial multiplication

--- crates/koblitz-curves/src/binary_field/field.rs
+++ crates/koblitz-curves/src/binary_field/field.rs
@@ -92,23 +92,56 @@ impl<const N: usize> Add for BigInt<N> {
 }
 
 // Algorithm 11.34 in "Handbook of Elliptic and HyperElliptic Curve Cryptography"
+// This is a naive algorithm of binary polynomial multiplication over a word
+impl<const N: usize> Mul<WORD> for BigInt<N> {
+    type Output = Self;
+
+    fn mul(self, rhs: WORD) -> Self::Output {
+        let mut c = Self::zero();
+        let mut word_mask = 1 as WORD;
+        let mut left = self;
+        for j in 0..WORD_BITS {
+            if (rhs & word_mask) == word_mask {
+                c = c + left;
+            }
+            if j != WORD_BITS - 1 {
+                left = left << 1;
+            }
+            word_mask <<= 1;
+        }
+        c
+    }
+}
+
+// Algorithm 11.37 in "Handbook of Elliptic and HyperElliptic Curve Cryptography"
+// This is a window-based optimized algorithm of binary polynomial multiplication,
+// set a small window size `k = 2`
 impl<const N: usize> Mul for BigInt<N> {
     type Output = Self;
 
     fn mul(self, rhs: Self) -> Self::Output {
+        // cache lookup table
+        let k = 2;
+        assert!(WORD_BITS % k == 0);
+        let capacity = 1 << k;
+        let mut lookup_table = Vec::with_capacity(capacity);
+        lookup_table.push(Self::zero());
+        for i in 1..capacity {
+            if i % 2 == 0 {
+                lookup_table.push(lookup_table[i / 2] << 1);
+            }
+            lookup_table.push(*lookup_table.last().unwrap() + rhs);
+        }
+        // iterate by window
         let mut c = Self::zero();
-        let mut word_mask = 1 as WORD;
-        let mut right = rhs;
-        for k in 0..WORD_BITS {
-            for j in 0..N {
-                if (self.0[j] & word_mask) == word_mask {
-                    c = c + (right << (j * WORD_BITS));
-                }
+        for j in (0..(WORD_BITS / k)).rev() {
+            for i in 0..N {
+                let chunk_word = (self.0[i] >> (j * k)) & ((capacity - 1) as WORD);
+                c = c + (lookup_table[chunk_word as usize] << (i * WORD_BITS));
             }
-            if k != WORD_BITS - 1 {
-                right = right << 1;
+            if j != 0 {
+                c = c << k;
             }
-            word_mask <<= 1;
         }
         c
     }
@@ -154,17 +187,22 @@ mod tests {
     use super::*;
     use std::str::FromStr;
 
+    // test bit string with big ending (high bit in the end) conversion from/to bigint
     #[test]
     fn test_bit_string() {
-        let v_bit_string = String::from_str("10011111011").unwrap();
-        let v = BigInt::<4>::from_bit_string(&v_bit_string);
-        let v_expected = BigInt([249, 6, 0, 0]);
-        assert_eq!(v, v_expected, "Test for BigInt::from_bit_string failed!");
-        assert_eq!(
-            v.to_bit_string(),
-            v_bit_string,
-            "Test for BigInt::to_bit_string failed!"
-        );
+        let test_data = [(
+            String::from_str("10011111011").unwrap(),
+            BigInt([249, 6, 0, 0]),
+        )];
+        for (v_bit_string, v_expected) in test_data {
+            let v = BigInt::<4>::from_bit_string(&v_bit_string);
+            assert_eq!(v, v_expected, "Test for BigInt::from_bit_string failed!");
+            assert_eq!(
+                v.to_bit_string(),
+                v_bit_string,
+                "Test for BigInt::to_bit_string failed!"
+            );
+        }
     }
 
     // Example 11.36 in "Handbook of Elliptic and HyperElliptic Curve Cryptography"
@@ -172,18 +210,20 @@ mod tests {
     // w(X) = u(X) * v(X) = X^15 + X^13 + X^10 + X^9 + X^7 + X^5 + X^2 + X
     #[test]
     fn test_bigint_mul() {
-        let (u_bit_string, v_bit_string, w_bit_string) = (
+        let test_data = vec![(
             String::from_str("011011").unwrap(),
             String::from_str("10011111011").unwrap(),
             String::from_str("0110010101100101").unwrap(),
-        );
-        let (u, v, w_expected) = (
-            BigInt::<4>::from_bit_string(&u_bit_string),
-            BigInt::<4>::from_bit_string(&v_bit_string),
-            BigInt::<4>::from_bit_string(&w_bit_string),
-        );
-        let w = u * v;
-        assert_eq!(w, w_expected, "Test for BigInt::mul failed!");
-        assert_eq!(w.to_bit_string(), w_bit_string);
+        )];
+        for (u_bit_string, v_bit_string, w_bit_string) in test_data {
+            let (u, v, w_expected) = (
+                BigInt::<4>::from_bit_string(&u_bit_string),
+                BigInt::<4>::from_bit_string(&v_bit_string),
+                BigInt::<4>::from_bit_string(&w_bit_string),
+            );
+            let w = u * v;
+            assert_eq!(w, w_expected, "Test for BigInt::mul failed!");
+            assert_eq!(w.to_bit_string(), w_bit_string);
+        }
     }
 }
