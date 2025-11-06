f0dfa19e122404575ae409208103439387408b75
PayneJoe <joepayne@163.com>
2025-11-06 23:54:18 +0800

    add bigint2 for binary polynomial

--- crates/koblitz-curves/src/binary_field/field.rs
+++ crates/koblitz-curves/src/binary_field/field.rs
@@ -4,8 +4,10 @@ use std::ops::{Add, Mul, Neg, Shl, Sub};
 // u8 word only for testing purpose, actually we will use u32 or u64 as one word
 pub type WORD = u8;
 pub const WORD_SIZE: usize = 8;
+// pub type WORD = u32;
+// pub const WORD_SIZE: usize = 32;
 // window size for caching when doing bigint multiplication
-pub const WINDOW_SIZE: usize = 2;
+pub const WINDOW_SIZE: usize = 4;
 
 #[allow(dead_code)]
 pub trait PolynomialSquaring: Sized {
@@ -37,9 +39,9 @@ impl PolynomialSquaring for u8 {
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
 pub struct BigInt<const N: usize>(pub [WORD; N]);
 
+#[allow(dead_code)]
 impl<const N: usize> BigInt<N> {
-    // convert bigint to bit string, for example: [249, 6] -> "10011111,011"
-    #[allow(dead_code)]
+    // convert bigint to bit string, for example: [249, 6] -> "10011111,011" when word = u8
     pub fn to_bit_string(&self) -> String {
         self.0
             .iter()
@@ -49,8 +51,7 @@ impl<const N: usize> BigInt<N> {
             .to_string()
     }
 
-    // convert bit string with bit-wise big-ending ordering (bits from left to right) to bigint, for example: "10011111,011" -> [249,6]
-    #[allow(dead_code)]
+    // convert bit string with bit-wise big-ending ordering (bits from left to right) to bigint, for example: "10011111,011" -> [249,6] when word = u8
     pub fn from_bit_string(s: &String) -> Self {
         assert!(s.len() <= WORD_SIZE * N);
         if s.len() == 0 {
@@ -75,7 +76,6 @@ impl<const N: usize> BigInt<N> {
 
     // modulus polynomial is assumed to be monic: X^N + h(x)
     // remove the leading one bit, so that we can obtain the residual polynomial h(x)
-    #[allow(dead_code)]
     pub fn strip_leading_one(&self) -> Self {
         if self.is_zero() {
             return *self;
@@ -95,21 +95,18 @@ impl<const N: usize> BigInt<N> {
         Self([0 as WORD; N])
     }
 
-    #[allow(dead_code)]
     pub fn one() -> Self {
         let mut result = Self::zero();
         result.0[0] = 1 as WORD;
         result
     }
 
-    #[allow(dead_code)]
     pub fn is_zero(&self) -> bool {
         self.0 == [0 as WORD; N]
     }
 
     // Algorithm 2.39 in "Gude to Elliptic Curve Cryptography"
-    #[allow(dead_code)]
-    pub fn squaring(&self) -> [Self; 2] {
+    pub fn squaring(&self) -> BigInt2<N> {
         let mut result = Vec::with_capacity(2 * N);
         // precomputation for byte squaring
         let capacity = 1 << 8;
@@ -123,13 +120,35 @@ impl<const N: usize> BigInt<N> {
             // multiple bytes in a word
             let word_bytes = self.0[i].to_be_bytes();
             for byte in word_bytes {
-                result.extend(lookup_table[byte as usize]);
+                let byte_squaring = lookup_table[byte as usize];
+                result.push(byte_squaring[0]);
+                result.push(byte_squaring[1]);
             }
         }
-        [
-            Self(result[..N].try_into().unwrap()),
-            Self(result[N..].try_into().unwrap()),
-        ]
+        let words = result
+            .chunks(WORD_SIZE / 8)
+            .map(|v| WORD::from_be_bytes(v.try_into().unwrap()))
+            .collect::<Vec<_>>();
+        BigInt2([
+            Self(words[..N].try_into().unwrap()),
+            Self(words[N..].try_into().unwrap()),
+        ])
+    }
+
+    pub fn shr_words(&self, n: usize) -> (Self, Self) {
+        assert!(n <= N);
+        (
+            Self(self.0[n..].try_into().unwrap()),
+            Self(self.0[..n].try_into().unwrap()),
+        )
+    }
+
+    pub fn shl_word(&self, n: usize) -> (Self, Self) {
+        assert!(n <= N);
+        (
+            Self(self.0[0..(N - n)].try_into().unwrap()),
+            Self(self.0[(N - n)..].try_into().unwrap()),
+        )
     }
 }
 
@@ -138,6 +157,18 @@ impl<const N: usize> From<WORD> for BigInt<N> {
         let mut result = Self::zero();
         result.0[0] = w;
         result
+    }
+}
+
+impl<const N: usize> From<Vec<WORD>> for BigInt<N> {
+    fn from(v: Vec<WORD>) -> Self {
+        if v.len() >= N {
+            Self(v[..N].try_into().unwrap())
+        } else {
+            let mut result = [0 as WORD; N];
+            result.copy_from_slice(&v);
+            Self(result)
+        }
     }
 }
 
@@ -156,11 +187,11 @@ impl<const N: usize> Add for BigInt<N> {
 // Algorithm 11.34 in "Handbook of Elliptic and HyperElliptic Curve Cryptography"
 // This is a naive algorithm of binary polynomial multiplication over a word
 impl<const N: usize> Mul<WORD> for BigInt<N> {
-    type Output = Self;
+    type Output = BigInt2<N>;
 
     // TODO: add a small lookup table would be a little bit helpful
     fn mul(self, rhs: WORD) -> Self::Output {
-        let mut c = Self::zero();
+        let mut c = BigInt2::<N>::zero();
         let mut word_mask = 1 as WORD;
         let mut left = self;
         for j in 0..WORD_SIZE {
@@ -178,24 +209,23 @@ impl<const N: usize> Mul<WORD> for BigInt<N> {
 
 // Algorithm 11.37 in "Handbook of Elliptic and HyperElliptic Curve Cryptography"
 // This is a window-based optimized algorithm of binary polynomial multiplication,
-// set a small window size `k = 2`
 impl<const N: usize> Mul for BigInt<N> {
-    type Output = Self;
+    type Output = BigInt2<N>;
 
     fn mul(self, rhs: Self) -> Self::Output {
         // cache lookup table
         assert!(WORD_SIZE % WINDOW_SIZE == 0);
         let capacity = 1 << WINDOW_SIZE;
-        let mut lookup_table = Vec::with_capacity(capacity);
-        lookup_table.push(Self::zero());
+        let mut lookup_table = vec![BigInt2::<N>::zero(); capacity];
         for i in 1..capacity {
             if i % 2 == 0 {
-                lookup_table.push(lookup_table[i / 2] << 1);
+                lookup_table[i] = lookup_table[i / 2] << 1;
+            } else {
+                lookup_table[i] = lookup_table[i - 1] + rhs;
             }
-            lookup_table.push(*lookup_table.last().unwrap() + rhs);
         }
         // iterate by window
-        let mut c = Self::zero();
+        let mut c = BigInt2::<N>::zero();
         for j in (0..(WORD_SIZE / WINDOW_SIZE)).rev() {
             for i in 0..N {
                 let chunk_word = (self.0[i] >> (j * WINDOW_SIZE)) & ((capacity - 1) as WORD);
@@ -229,6 +259,73 @@ impl<const N: usize> Shl<usize> for BigInt<N> {
     }
 }
 
+impl<const N: usize> Neg for BigInt<N> {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        self
+    }
+}
+
+impl<const N: usize> Sub<Self> for BigInt<N> {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        self + rhs
+    }
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub struct BigInt2<const N: usize>(pub [BigInt<N>; 2]);
+
+impl<const N: usize> BigInt2<N> {
+    pub fn zero() -> Self {
+        Self([BigInt::<N>::zero(), BigInt::<N>::zero()])
+    }
+}
+
+impl<const N: usize> Add<Self> for BigInt2<N> {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self([self.0[0] + rhs.0[0], self.0[1] + rhs.0[1]])
+    }
+}
+
+impl<const N: usize> Add<BigInt<N>> for BigInt2<N> {
+    type Output = Self;
+
+    fn add(self, rhs: BigInt<N>) -> Self::Output {
+        Self([self.0[0] + rhs, self.0[1]])
+    }
+}
+
+impl<const N: usize> Shl<usize> for BigInt2<N> {
+    type Output = Self;
+
+    fn shl(self, shift: usize) -> Self::Output {
+        let mut result = vec![0 as WORD; 2 * N];
+        let word_shift = shift / WORD_SIZE;
+        let bit_shift = shift % WORD_SIZE;
+
+        let mut words = self.0[0].0.to_vec();
+        words.extend_from_slice(&self.0[1].0);
+
+        for i in (0..(2 * N)).rev() {
+            if i >= word_shift {
+                result[i] = words[i - word_shift] << bit_shift;
+                if bit_shift > 0 && i - word_shift > 0 {
+                    result[i] |= words[i - word_shift - 1] >> (WORD_SIZE - bit_shift);
+                }
+            }
+        }
+        Self([
+            BigInt(result[..N].try_into().unwrap()),
+            BigInt(result[N..].try_into().unwrap()),
+        ])
+    }
+}
+
 #[allow(dead_code)]
 pub trait BinaryField<const N: usize>:
     Debug
@@ -241,8 +338,12 @@ pub trait BinaryField<const N: usize>:
     + Mul<Self, Output = Self>
     + Neg<Output = Self>
 {
-    const MODULUS: BigInt<N>;
-    fn reduce(element: BigInt<N>) -> Self;
+    // irreducible binary polynomial: f(X) = X^M + R(X) where M is the degree of binary polynomial, and R(X) is residual polynomial
+    // which M < N * WORD_SIZE, and deg(R) < M
+    const M: usize;
+    const R: BigInt<N>;
+    const UK: [BigInt<N>; WORD_SIZE];
+    fn reduce(element: BigInt2<N>) -> Self;
 }
 
 #[cfg(test)]
@@ -277,16 +378,23 @@ mod tests {
             String::from_str("011011").unwrap(),
             String::from_str("10011111011").unwrap(),
             String::from_str("0110010101100101").unwrap(),
+            String::from_str("").unwrap(),
         )];
-        for (u_bit_string, v_bit_string, w_bit_string) in test_data {
+        for (u_bit_string, v_bit_string, w_low_bit_string, w_high_bit_string) in test_data {
             let (u, v, w_expected) = (
                 BigInt::<4>::from_bit_string(&u_bit_string),
                 BigInt::<4>::from_bit_string(&v_bit_string),
-                BigInt::<4>::from_bit_string(&w_bit_string),
+                BigInt2([
+                    BigInt::<4>::from_bit_string(&w_low_bit_string),
+                    BigInt::<4>::from_bit_string(&w_high_bit_string),
+                ]),
             );
             let w = u * v;
             assert_eq!(w, w_expected, "Test for BigInt::mul failed!");
-            assert_eq!(w.to_bit_string(), w_bit_string);
+            assert!(
+                (w.0[0].to_bit_string() == w_low_bit_string)
+                    && (w.0[1].to_bit_string() == w_high_bit_string)
+            );
         }
     }
 
@@ -322,10 +430,10 @@ mod tests {
         for (v_bit_string, v_squaring_bit_string) in test_data {
             let (v, v_squaring_expected) = (
                 BigInt::<2>::from_bit_string(&v_bit_string),
-                [
+                BigInt2([
                     BigInt::<2>::from_bit_string(&v_squaring_bit_string[0]),
                     BigInt::<2>::from_bit_string(&v_squaring_bit_string[1]),
-                ],
+                ]),
             );
             let v_squaring = v.squaring();
             assert_eq!(
--- crates/koblitz-curves/src/binary_field/fq.rs
+++ crates/koblitz-curves/src/binary_field/fq.rs
@@ -1,23 +1,26 @@
-use crate::binary_field::field::{BigInt, BinaryField, WORD, WORD_SIZE};
+use crate::binary_field::field::{BigInt, BigInt2, BinaryField, WORD_SIZE};
 use core::ops::{Add, Mul, Neg, Shl, Sub};
 
-// Fq = GF(2^m)
-#[derive(Clone, Copy, PartialEq, Eq, Debug)]
-pub struct Fq<const N: usize>(BigInt<N>);
+// binary field Fq = GF(2^m) / f(X), where m = 163
+// N = 21 when word = u8
+pub const M: usize = 163;
+pub const N: usize = 21;
 
-impl<const N: usize> Fq<N> {
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub struct Fq(BigInt<N>);
+
+#[allow(dead_code)]
+impl Fq {
     pub fn zero() -> Self {
-        Self(BigInt([0 as WORD; N]))
+        Self(BigInt::<N>::zero())
     }
 
     pub fn one() -> Self {
-        let mut one = [0 as WORD; N];
-        one[N - 1] = 1 as WORD;
-        Self(BigInt(one))
+        Self(BigInt::<N>::one())
     }
 }
 
-impl<const N: usize> Add for Fq<N> {
+impl Add for Fq {
     type Output = Self;
 
     fn add(self, rhs: Self) -> Self::Output {
@@ -25,7 +28,7 @@ impl<const N: usize> Add for Fq<N> {
     }
 }
 
-impl<const N: usize> Shl<usize> for Fq<N> {
+impl Shl<usize> for Fq {
     type Output = Self;
 
     fn shl(self, shift: usize) -> Self::Output {
@@ -33,17 +36,64 @@ impl<const N: usize> Shl<usize> for Fq<N> {
     }
 }
 
-impl<const N: usize> Mul for Fq<N> {
+impl Mul for Fq {
     type Output = Self;
 
     fn mul(self, rhs: Self) -> Self::Output {
-        Self(self.0 * rhs.0)
+        Self::reduce(self.0 * rhs.0)
     }
 }
 
-// impl<const N: usize> BinaryField<N> for Fq<N> {
-//     const MODULUS: INTEGER<N> = [0; N];
-//     fn reduce(element: INTEGER<N>) -> Self {
-//         Fq
-//     }
-// }
+impl Neg for Fq {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        Self(-self.0)
+    }
+}
+
+impl Sub<Self> for Fq {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self(self.0 - rhs.0)
+    }
+}
+
+impl BinaryField<N> for Fq {
+    const M: usize = M;
+    const R: BigInt<N> = BigInt([
+        201, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    ]);
+    const UK: [BigInt<N>; WORD_SIZE] = [
+        BigInt([
+            201, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        ]),
+        BigInt([
+            146, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        ]),
+        BigInt([
+            36, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        ]),
+        BigInt([
+            72, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        ]),
+        BigInt([
+            144, 12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        ]),
+        BigInt([
+            32, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        ]),
+        BigInt([
+            64, 50, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        ]),
+        BigInt([
+            128, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        ]),
+    ];
+
+    // Algorithm 2.40 in "Guide to Elliptic Curve Cryptography"
+    fn reduce(ele: BigInt2<N>) -> Self {
+        todo!()
+    }
+}
